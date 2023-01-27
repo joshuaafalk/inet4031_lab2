48243d071a03a72970eea9fa7880fcd9bda8b8aa
Joshua <jfalk314@gmail.com>
2023-01-27 02:53:57 +0000

    start of part2, need c script

--- hello.py
+++ hello.py
@@ -4,4 +4,6 @@ b =2
 c =a+b
 print("Python says: Hello, World!")
 print("%s + %s = %s" % (a,b,c))
-
+users = ["User1", "User2", "User3"]
+for item in users:
+    print(item)
--- hello.sh
+++ hello.sh
@@ -4,4 +4,9 @@ b=2
 c=$((a+b))
 echo "Bash says: Hello, World!"
 echo "$a + $b = $c"
+users=("User1" "User2" "User3")
+for var in ${users[@]};
+do
+    echo $var
+done
 
